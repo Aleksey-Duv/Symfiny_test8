71a390b709e336c34c6ff77370413318446946fd
Aleksey-Duv <alexey.duvanov@inlinegroup-c.ru>
2023-05-12 15:08:12 +0300

    ww

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use http\Client\Curl\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 
 class MainController extends AbstractController
 {
@@ -17,11 +21,32 @@ class MainController extends AbstractController
     }
 
     #[Route('/getName/{id}', name: 'app_getName')]
-    public function getName(int $id): Response
+    public function getName(int $id, PersistenceManagerRegistry $doctrine): Response
     {
-        $name = 'Имя не найдено';
-//      $rr =  $this->u getName($id);
+        $name = 'Пользователь отсутствует';
+        $User = $doctrine->getRepository(Users::class)->find($id);
+        if (isset($User)) {
+            $name = $User->getUserName() ;
+        }
+//dump($User);die;
 
-        return new Response( $name );
+        return new Response($name );
+    }
+
+    #[Route('/setUser', name: 'app_setUser')]
+    public function setUser( PersistenceManagerRegistry $doctrine ): Response
+    {
+       $doctrineManager = $doctrine->getManager();
+
+       $user = new Users();
+        $user->setUserId(2)->setUserName('Иванов Пётр');
+        $doctrineManager->persist($user);
+
+        $doctrineManager->flush();
+
+
+
+
+        return new Response( 'сохранено' );
     }
 }
